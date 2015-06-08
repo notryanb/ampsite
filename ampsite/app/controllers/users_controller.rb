ff127a46bc9b82b161f55248907150e7b54ea479
notryanb <notryanb@gmail.com>
2015-06-08 10:57:06 -0400

    Add generic user model and user controller skeleton.

---
+++ ampsite/app/controllers/users_controller.rb
@@ -0,0 +1,40 @@
+class UsersController < ApplicationController
+
+  def index
+  end
+
+  # GET Create User
+  def new
+  end
+
+  # POST create User
+  def create
+  end
+
+  # For user Profile
+  def show
+  end
+
+  # For seeing all users
+  def all
+  end
+
+  # GET Editing user profile
+  def edit
+  end
+
+  # POST Updates
+  def update
+  end
+
+  # create a user session
+  def login
+  end
+
+  # end a user session
+  def logout
+  end
+
+
+
+end
