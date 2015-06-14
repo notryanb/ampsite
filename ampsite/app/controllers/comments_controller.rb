9529409b4da931c4b0954982dad55b87a92c58da
notryanb <notryanb@gmail.com>
2015-06-14 14:22:44 -0400

    New Comment Route works

--- ampsite/app/controllers/comments_controller.rb
+++ ampsite/app/controllers/comments_controller.rb
@@ -1,11 +1,24 @@
 class CommentsController < ApplicationController
+  
   def show
   end
 
   def new
+    @post_id = params[:post_id]
+    @comment = Comment.new
+    render 'new'
   end
 
   def create
+    @comment = Comment.new(comment_params)
+    @post = Post.find_by(id: @comment.post_id)
+    @topic = @post.topic
+    if @comment.save
+      redirect_to topic_post_path(@topic, @post)
+    else
+      render action: :new
+    end
+
   end
 
   def edit
@@ -18,5 +31,11 @@ class CommentsController < ApplicationController
   end
 
   private
+
+  def comment_params
+    params.require(:comment).permit(:user_id, 
+                                    :post_id,
+                                    :content)
+  end
   
 end
