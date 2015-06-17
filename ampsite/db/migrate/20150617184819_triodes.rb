94de9ddcfe6a661e40c65ee7d28df49e4d33112d
notryanb <notryanb@gmail.com>
2015-06-17 15:16:38 -0400

    Create Columns for tube type tables

--- ampsite/db/migrate/20150617184819_triodes.rb
+++ ampsite/db/migrate/20150617184819_triodes.rb
@@ -1,5 +1,15 @@
 class Triode < ActiveRecord::Migration
   def change
-    create_table :triodes do
+    create_table :triodes do |t|
+      t.string :identifier, null: false
+      t.string :classification, null: false
+      t.integer :filament_voltage, null: false
+      t.integer :filament_current, null: false
+      t.integer :max_anode_voltage, null: false
+      t.integer :anode_dissipation, null: false
+      t.integer :amplification_factor, null: false
+
+      t.timestamps
+    end
   end
 end
