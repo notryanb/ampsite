54db6ba32204a62e1b41e5de7403f1cb73b25058
notryanb <notryanb@gmail.com>
2015-06-18 15:38:29 -0400

    Preamp create path

--- ampsite/app/controllers/triodes_controller.rb
+++ ampsite/app/controllers/triodes_controller.rb
@@ -7,12 +7,34 @@ class TriodesController < ApplicationController
   end
 
   def create
+    @triode = Triode.new(triode_params)
+    if @triode.save
+      redirect_to preamps_path
+    else
+      render action: :new
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def triode_params
+    params.require(:triode).permit(:identifier,
+                                  :max_anode_voltage,
+                                  :max_filament_cathode_voltage,
+                                  :max_cathode_current,
+                                  :anode_dissipation,
+                                  :amplification_factor,
+                                  :filament_voltage,
+                                  :filament_current,
+                                  :pinout,
+                                  :description,
+                                  :notes)
   end
 
 end
