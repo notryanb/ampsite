6ccebdd960059cb8ec88a04c94b47e162accd7be
notryanb <notryanb@gmail.com>
2015-06-19 21:00:59 -0400

    Fix Pentode migration by adding identifier and view page fixed with nil statement

--- ampsite/app/controllers/pentodes_controller.rb
+++ ampsite/app/controllers/pentodes_controller.rb
@@ -7,12 +7,34 @@ class PentodesController < ApplicationController
   end
 
   def create
+    @pentode = Pentode.new(pentode_params)
+    if @triode.save
+      redirect_to preamps_path
+    else
+      render action: :new
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def pentode_params
+    params.require(:pentode).permit(:identifier,
+                                  :max_anode_voltage
+                                  :max_grid2_voltage
+                                  :max_anode_dissipation
+                                  :max_grid2_dissipation
+                                  :max_cathode_current
+                                  :filament_voltage
+                                  :filament_current
+                                  :pinout,
+                                  :description,
+                                  :notes)
   end
 
 end
