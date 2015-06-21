0464af4ac2ac510f27a548d71daa3caaee35f881
notryanb <notryanb@gmail.com>
2015-06-21 13:26:07 -0400

    Add show, edit, and updates routes to tetrodes

--- ampsite/app/controllers/tetrodes_controller.rb
+++ ampsite/app/controllers/tetrodes_controller.rb
@@ -7,15 +7,53 @@ class TetrodesController < ApplicationController
   end
 
   def create
+    @tetrode = Tetrode.new(tetrode_params)
+    if @tetrode.save
+      redirect_to powers_path
+    else
+      render action: :new
+    end
+  end
+
+  def show
+    @tetrode = Tetrode.find_by(params[:id])
+    render 'show'
+  end
+
+  def edit
+    @tetrode = Tetrode.find_by(params[:id])
+    render 'edit'
   end
 
   def update
+    @tetrode = Tetrode.find_by(params[:id])
+    if @tetrode.update_attribute(tetrode_params)
+      render 'show'
+    else
+      render action: :edit
+    end
   end
 
   def destroy
     @tetrode = Tetrode.find_by(params[:id])
     @tetrode.destroy
     render 'powers'
+  end
+
+  private
+
+  def tetrode_params
+    params.require(:tetrode).permit(:identifier,
+                                  :max_anode_voltage
+                                  :max_grid2_voltage
+                                  :max_anode_dissipation
+                                  :max_grid2_dissipation
+                                  :max_cathode_current
+                                  :filament_voltage
+                                  :filament_current
+                                  :pinout,
+                                  :description,
+                                  :notes)
   end
 
 end
