e56f708bb177f62f1bce9df98a87ab982e38ce66
notryanb <notryanb@gmail.com>
2015-06-24 13:18:25 -0400

    Add polymorphic association for datasheets and tube-types

--- ampsite/db/migrate/20150617174810_datasheets.rb
+++ ampsite/db/migrate/20150617174810_datasheets.rb
@@ -1,10 +1,11 @@
 class Datasheets < ActiveRecord::Migration
   def change
     create_table :datasheets do |t|
+      t.references :datasheetable, polymorphic: true, index: true
       t.string :url
-      t.references :tube, null: false
 
       t.timestamps
     end
+    add_index :datasheets, [:datasheetable_id, :datasheetable_type]
   end
 end
