c2b9cce3b09fbd7fa1715569c49a0e1f23f419a7
notryanb <notryanb@gmail.com>
2015-06-27 17:58:50 -0400

    Add Socket Type column to tube listing and created class method to display if there the socket isn't nil.

--- ampsite/app/models/tetrode.rb
+++ ampsite/app/models/tetrode.rb
@@ -3,4 +3,11 @@ class Tetrode < ActiveRecord::Base
   has_one :pinout, as: :pinoutable
   has_one :tubesocket, through: :pinout
 
+  def tubesocket?
+    if self.tubesocket
+      return self.tubesocket
+    else
+      return "NA"
+    end 
+  end
 end
